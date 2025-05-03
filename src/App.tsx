3a198aae87b63aef039b94b2451fbf158cad5558
Michael Cambria <michael.v.cambria@gmail.com>
2025-05-02 21:26:15 -0700

    catch keydown to stop annoying firefox behavior

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import { useMemo, useState } from 'react';
 import TabEditor from './editor/editor';
 
+const handleKeyDown = (e: React.KeyboardEvent) => {
+  switch (e.key) {
+    case "Backspace":
+      e.preventDefault();
+      break;
+  }
+}
+
 function App() {
   return (
-    <div className="min-h-screen p-8 bg-gray-900">
+    <div className="min-h-screen p-8 bg-gray-900" onKeyDown={handleKeyDown}>
       <div className="max-w-4xl mx-auto">
         <h1 className="text-3xl font-bold mb-8 text-white">KeyTabs</h1>
         <div className="bg-gray-800 rounded-lg shadow-lg p-6">
