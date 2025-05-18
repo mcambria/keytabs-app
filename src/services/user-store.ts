f1a726b205d91fb74dabd9ec5f4c3aff8b618e40
Michael Cambria <michael.v.cambria@gmail.com>
2025-05-18 10:21:26 -0700

    add preference mappings - currently unused

--- src/services/user-store.ts
+++ src/services/user-store.ts
@@ -1,23 +1,37 @@
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 
-export type User = { id: string }
+export type User = { id: string, preferences: Preferences }
+export type Preferences = { tabsListCollapsed: boolean, keybingingsCollapsed: boolean }
 
-type UserState = {
-    user: User;
-    saveUser: (User: User) => void;
+type UserStoreState = {
+    currentUser: User;
 }
+
+type UserStoreStateActions = {
+    updatePreferences: (updates: Partial<Preferences>) => void;
+}
+
+type UserState = UserStoreState & UserStoreStateActions;
 
 export const useUserStore = create<UserState>()(
     persist(
         (set) => ({
-            user: { id: crypto.randomUUID() },
-            saveUser: (user) => {
-                set({ user: user});
+            currentUser: { id: crypto.randomUUID(), preferences: { tabsListCollapsed: false, keybingingsCollapsed: false } },
+            updatePreferences: (updates: Partial<Preferences>) => {
+                set((state) => ({
+                    currentUser: {
+                        ...state.currentUser,
+                        preferences: {
+                            ...state.currentUser.preferences,
+                            ...updates,
+                        },
+                    },
+                }));
             }
         }),
         {
-            name: 'current-user'
+            name: 'current-user',
         }
     )
 )
