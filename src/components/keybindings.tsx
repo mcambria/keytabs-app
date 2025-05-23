140b3f414ee21447dfd2874257ed5c3ff20be2a2
Michael Cambria <michael.v.cambria@gmail.com>
2025-05-22 22:37:56 -0700

    refactor to use fc for keybinding item

--- src/components/keybindings.tsx
+++ src/components/keybindings.tsx
@@ -6,6 +6,18 @@ type KeyBindingsProps = {
   className?: string;
 };
 
+type KeybindingItemProps = {
+  label: string;
+  keybinding: string;
+};
+
+const KeybindingItem: React.FC<KeybindingItemProps> = ({ label, keybinding }) => (
+  <li className="flex justify-between">
+    <span>{label}</span>
+    <kbd className="px-2 py-1 bg-ide-kbd rounded text-ide-text-accent-subtle">{keybinding}</kbd>
+  </li>
+);
+
 const Keybindings: React.FC<KeyBindingsProps> = ({ className = "" }) => {
   return (
     <CollapsiblePanel
@@ -18,92 +30,35 @@ const Keybindings: React.FC<KeyBindingsProps> = ({ className = "" }) => {
         <div className="mb-4">
           <h3 className="text-lg font-medium mb-2 text-ide-text-accent-primary">Navigation</h3>
           <ul className="space-y-2">
-            <li className="flex justify-between">
-              <span>Move Left</span>
-              <kbd className="px-2 py-1 bg-ide-kbd rounded text-ide-text-accent-subtle">←</kbd>
-            </li>
-            <li className="flex justify-between">
-              <span>Move Right</span>
-              <kbd className="px-2 py-1 bg-ide-kbd rounded text-ide-text-accent-subtle">→</kbd>
-            </li>
-            <li className="flex justify-between">
-              <span>Move Up</span>
-              <kbd className="px-2 py-1 bg-ide-kbd rounded text-ide-text-accent-subtle">↑</kbd>
-            </li>
-            <li className="flex justify-between">
-              <span>Move Down</span>
-              <kbd className="px-2 py-1 bg-ide-kbd rounded text-ide-text-accent-subtle">↓</kbd>
-            </li>
-            <li className="flex justify-between">
-              <span>Move Left (8 chords)</span>
-              <kbd className="px-2 py-1 bg-ide-kbd rounded text-ide-text-accent-subtle">Ctrl + ←</kbd>
-            </li>
-            <li className="flex justify-between">
-              <span>Move Right (8 chords)</span>
-              <kbd className="px-2 py-1 bg-ide-kbd rounded text-ide-text-accent-subtle">Ctrl + →</kbd>
-            </li>
-            <li className="flex justify-between">
-              <span>Move Up (1 line)</span>
-              <kbd className="px-2 py-1 bg-ide-kbd rounded text-ide-text-accent-subtle">Ctrl + ↑</kbd>
-            </li>
-            <li className="flex justify-between">
-              <span>Move Down (1 line)</span>
-              <kbd className="px-2 py-1 bg-ide-kbd rounded text-ide-text-accent-subtle">Ctrl + ↓</kbd>
-            </li>
+            <KeybindingItem label="Move Left" keybinding="←" />
+            <KeybindingItem label="Move Right" keybinding="→" />
+            <KeybindingItem label="Move Up" keybinding="↑" />
+            <KeybindingItem label="Move Down" keybinding="↓" />
+            <KeybindingItem label="Move Left (8 chords)" keybinding="Ctrl + ←" />
+            <KeybindingItem label="Move Right (8 chords)" keybinding="Ctrl + →" />
+            <KeybindingItem label="Move Up (1 line)" keybinding="Ctrl + ↑" />
+            <KeybindingItem label="Move Down (1 line)" keybinding="Ctrl + ↓" />
           </ul>
         </div>
         <div className="mb-4">
           <h3 className="text-lg font-medium mb-2 text-ide-text-accent-primary">Editing</h3>
           <ul className="space-y-2">
-            <li className="flex justify-between">
-              <span>Insert Bar Line</span>
-              <kbd className="px-2 py-1 bg-ide-kbd rounded text-ide-text-accent-subtle">|</kbd>
-            </li>
-            <li className="flex justify-between">
-              <span>Toggle Cell Edit</span>
-              <kbd className="px-2 py-1 bg-ide-kbd rounded text-ide-text-accent-subtle">Enter</kbd>
-            </li>
-            <li className="flex justify-between">
-              <span>Insert Chord</span>
-              <kbd className="px-2 py-1 bg-ide-kbd rounded text-ide-text-accent-subtle">Shift + Enter</kbd>
-            </li>
-            <li className="flex justify-between">
-              <span>Insert Line</span>
-              <kbd className="px-2 py-1 bg-ide-kbd rounded text-ide-text-accent-subtle">Ctrl + Enter</kbd>
-            </li>
-            <li className="flex justify-between">
-              <span>Delete Backward</span>
-              <kbd className="px-2 py-1 bg-ide-kbd rounded text-ide-text-accent-subtle">Backspace</kbd>
-            </li>
-            <li className="flex justify-between">
-              <span>Delete</span>
-              <kbd className="px-2 py-1 bg-ide-kbd rounded text-ide-text-accent-subtle">Delete</kbd>
-            </li>
-            <li className="flex justify-between">
-              <span>Delete Chord</span>
-              <kbd className="px-2 py-1 bg-ide-kbd rounded text-ide-text-accent-subtle">Shift + Backspace</kbd>
-            </li>
-            <li className="flex justify-between">
-              <span>Delete Line</span>
-              <kbd className="px-2 py-1 bg-ide-kbd rounded text-ide-text-accent-subtle">Ctrl + Backspace</kbd>
-            </li>
+            <KeybindingItem label="Insert Bar Line" keybinding="|" />
+            <KeybindingItem label="Toggle Cell Edit" keybinding="Enter" />
+            <KeybindingItem label="Insert Chord" keybinding="Shift + Enter" />
+            <KeybindingItem label="Insert Line" keybinding="Ctrl + Enter" />
+            <KeybindingItem label="Delete Backward" keybinding="Backspace" />
+            <KeybindingItem label="Delete" keybinding="Delete" />
+            <KeybindingItem label="Delete Chord" keybinding="Shift + Backspace" />
+            <KeybindingItem label="Delete Line" keybinding="Ctrl + Backspace" />
           </ul>
         </div>
         <div className="mb-4">
           <h3 className="text-lg font-medium mb-2 text-ide-text-accent-primary">Selection</h3>
           <ul className="space-y-2">
-            <li className="flex justify-between">
-              <span>Extend Selection</span>
-              <kbd className="px-2 py-1 bg-ide-kbd rounded text-ide-text-accent-subtle">Shift + Arrow Keys</kbd>
-            </li>
-            <li className="flex justify-between">
-              <span>Copy Selection</span>
-              <kbd className="px-2 py-1 bg-ide-kbd rounded text-ide-text-accent-subtle">Ctrl + C</kbd>
-            </li>
-            <li className="flex justify-between">
-              <span>Paste</span>
-              <kbd className="px-2 py-1 bg-ide-kbd rounded text-ide-text-accent-subtle">Ctrl + V</kbd>
-            </li>
+            <KeybindingItem label="Extend Selection" keybinding="Shift + Arrow Keys" />
+            <KeybindingItem label="Copy Selection" keybinding="Ctrl + C" />
+            <KeybindingItem label="Paste" keybinding="Ctrl + V" />
           </ul>
         </div>
       </div>
