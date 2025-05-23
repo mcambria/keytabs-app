6ad0cccbb2d8f19172bf09f74971ceb6f3b19b9d
Michael Cambria <michael.v.cambria@gmail.com>
2025-05-22 22:27:01 -0700

    add warning banner

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,11 @@ function App() {
       <div className="flex h-screen">
         <TabsList className="h-full" />
         <div className="flex-1 flex flex-col h-full ">
-          <div className="p-4 border-b border-ide-highlight flex-none">
+          <div className="p-4 border-b border-ide-highlight flex-none flex space-x-4">
             <h1 className="text-xl font-medium text-ide-text-accent-primary">KeyTabs</h1>
+            <div className="bg-ide-text-destructive/10 text-ide-text-destructive text-sm px-2 py-1 rounded mb-2 inline-block">
+              ⚠️ Under Development - Your tabs may be deleted
+            </div>
           </div>
           <TabEditor className="flex-1 p-4" />
         </div>
